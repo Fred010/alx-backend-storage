d06083c82193730ea12a2515a01c6b18caff666c
Fred010 <fjbaafi18@gmail.com>
2024-10-25 21:28:09 +0000

    Top students

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,22 @@
+def top_students(mongo_collection):
+    """
+    Returns all students sorted by their average score.
+    """
+    # Using MongoDB's aggregation framework to compute average and sort
+    pipeline = [
+        {
+            "$addFields": {
+                "averageScore": {
+                    "$avg": "$topics.score"
+                }
+            }
+        },
+        {
+            "$sort": {
+                "averageScore": -1
+            }
+        }
+    ]
+
+    # Run the aggregation pipeline
+    return list(mongo_collection.aggregate(pipeline))
