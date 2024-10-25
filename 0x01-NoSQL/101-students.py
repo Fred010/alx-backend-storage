bf61ce6bd5d154dff9028bed2e31393bc5004fe5
Fred010 <fjbaafi18@gmail.com>
2024-10-25 21:42:55 +0000

    Top students update

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+""" returns all students sorted by average score
+"""
+
+
 def top_students(mongo_collection):
     """
     Returns all students sorted by their average score.
